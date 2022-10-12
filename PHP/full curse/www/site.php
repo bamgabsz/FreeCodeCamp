403dded794fcfc7e98e1474e02b4ddce3d6195e1
bamgabsz <bamgabsz@gmail.com>
2022-10-12 15:08:44 -0300

    trabalhando com numeros

--- PHP/full curse/www/site.php
+++ PHP/full curse/www/site.php
@@ -9,7 +9,42 @@
 <body>
 
     <?php 
+        echo 40;
+        echo -40;
+        echo -20.0333;
+
+        echo 5 + 9;
+        echo 5 - 9;
+        echo 5 * 9;
+        echo 5 / 9;
+
+        // resto 1
+        echo 5 % 9;
+
+        echo (4 + 5) * 10;
         
+        $num = 0;
+        $num++;
+        echo $num;
+
+        //100
+        echo abs(-100);
+
+        //16
+        echo pow(2, 4);
+
+        //12
+        echo sqrt(144); 
+
+        echo max(2,10); //10
+
+        echo min(2,10); //2
+
+        echo round(3.2); //3 arredonda
+
+        echo ceil(3.3); // 4
+
+        echo floor(3.9); //3 
     ?>
 
 </body>

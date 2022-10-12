48b421fd3de759853bf0cf10c03bb3161333139d
bamgabsz <bamgabsz@gmail.com>
2022-10-12 19:58:34 -0300

    testando o css junto ao php

--- PHP/full curse/www/site.php
+++ PHP/full curse/www/site.php
@@ -4,22 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
 
-    <form action="site.php" method="post">
-
-    Maças: <input type="checkbox" name="frutas[]" value="apples"><br>
-    Laranjas: <input type="checkbox" name="frutas[]" value="oranges"><br>
-    Peras: <input type="checkbox" name="frutas[]" value="pears"><br>
-    <input type="submit">
-    </form>
-
-    <?php 
-        $fruits = $_POST["frutas"];
-        echo $fruits[0];
-    ?>
+    <p class="test">Olá mundo!</p>
 
 </body>
 </html>

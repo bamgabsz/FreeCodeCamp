a5ca883d8034d401e8f34e3e8fc98df4fb3470c5
bamgabsz <bamgabsz@gmail.com>
2022-10-12 14:18:48 -0300

    test

--- PHP/full curse/www/site.php
+++ PHP/full curse/www/site.php
@@ -9,35 +9,7 @@
 <body>
 
     <?php 
-       
-        $frase = "Academia da Girafa";
-
-        // funções/metodos em strings
-
-        // tudo pra minusculo
-        echo strtolower($frase);
-        echo "<br>";
-        // tudo pra maiusculo
-        echo strtoupper($frase);
-        echo "<br>";
-        // tamanho da string
-        echo strlen($frase);
-        echo "<br>";
-        // pegar uma determinada letra
-        echo $frase[1];
-        echo "<br>";
-
-        $frase[1] = "8";
-        echo $frase; 
-        echo "<br>";
-
-        echo str_replace("Girafa","Panda", $frase);
-        echo "<br>";
-
-        echo str_replace("rafa","ro", $frase);
-        echo "<br>";
-
-        echo substr($frase,8,3);
+        
     ?>
 
 </body>

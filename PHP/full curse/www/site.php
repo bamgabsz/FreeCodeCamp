35fb0b88de27c4f3bd18ff18dcf4120c4edc024c
bamgabsz <bamgabsz@gmail.com>
2022-10-12 20:40:12 -0300

    functions

--- PHP/full curse/www/site.php
+++ PHP/full curse/www/site.php
@@ -1,26 +1,21 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>php</title>
+    <title>document</title>
 </head>
 <body>
+    
+<?php
 
-    <form action="site.php" method="post">
-        <input type="text" name="aluno">
-        <input type="submit">
-    </form>
+    function sayHi($name){
+        echo "Hello $name";
+    }
 
-    <?php 
-        $classe = array("Jim"=>"A+", "Pam"=>"B-","Oscar"=>"C+");
-        echo $classe[$_POST["aluno"]]; 
-        //$classe["Jim"] = "F";
-        //echo $classe["Jim"];
-        //echo count($classe);
-    ?>
-    <br>
+    sayHi("mike");
+?>
+
 </body>
 </html>

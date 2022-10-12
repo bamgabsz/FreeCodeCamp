977107311fa1f2de198f48cb863937fd3a37cdff
bamgabsz <bamgabsz@gmail.com>
2022-10-12 20:23:10 -0300

    Array associativa 'x'=>'2'

--- PHP/full curse/www/site.php
+++ PHP/full curse/www/site.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
+    <title>php</title>
 </head>
 <body>
 
-    <p class="test">Olá mundo!</p>
+    <form action="site.php" method="post">
+        <input type="text" name="aluno">
+        <input type="submit">
+    </form>
 
+    <?php 
+        $classe = array("Jim"=>"A+", "Pam"=>"B-","Oscar"=>"C+");
+        echo $classe[$_POST["aluno"]]; 
+        //$classe["Jim"] = "F";
+        //echo $classe["Jim"];
+        //echo count($classe);
+    ?>
+    <br>
 </body>
 </html>

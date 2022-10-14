47b9f822566e25af30aa21d7cd70375aa6cbaa8e
bamgabsz <bamgabsz@gmail.com>
2022-10-14 14:23:33 -0300

    if statements - 2

--- PHP/full curse/www/site.php
+++ PHP/full curse/www/site.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>document</title>
 </head>
+
 <body>
-    
-<?php
-        $isMale = true;
 
-    if ($isMale == true) {
-        echo "É macho";
-    } elseif($isMale == !true) {
-        echo "É macha";  
+    <?php
+    function getMax($num1, $num2)
+    {
+        if ($num1 > $num2) {
+            return $num1;
+        } else {
+            return $num2;
+        }
     }
-
-?>
+    ?>
 
 </body>
+
 </html>
